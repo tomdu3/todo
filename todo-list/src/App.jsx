7498caa8b0c1b0174e4c771cac8b3cc604474038
Tomislav Dukez <tomdu3@ymail.com>
2024-02-21 05:53:00 +0000

    Add delete func

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -21,6 +21,24 @@ export default function App() {
     setNewItem('');
   }
 
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          // cannot mutate state directly like: todo.completed = !todo.completed
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -37,10 +55,12 @@ export default function App() {
             // key is required when returning multiple elements
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input type="checkbox" checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)} />
                 <span>{todo.title}</span>
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger">Delete</button>
             </li>
           )
         })}
