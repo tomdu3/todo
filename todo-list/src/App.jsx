acba6080c516e8b4ee48cabba7e563c82b441daf
Tomislav Dukez <tomdu3@ymail.com>
2024-02-21 06:28:46 +0000

    Add component newTodoForm

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,24 +1,22 @@
 import { useState } from 'react'
+import { NewTodoForm } from './components/newTodoForm'
 import './styles.css'
 
 export default function App() {
-  const [newItem, setNewItem] = useState('')
   const [todos, setTodos] = useState([])
 
-  function handleSubmit(e) {
-    e.preventDefault()
+  function addTodo(title) {
 
     // anytime we want to use the current value, we need to pass it as a function
     setTodos((currentTodos) => {
       return [...currentTodos,
         {
           id: crypto.randomUUID(),
-          title: newItem,
+          title,
           completed: false
         },
       ]
     });
-    setNewItem('');
   }
 
   function toggleTodo(id, completed) {
@@ -41,13 +39,7 @@ export default function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input value={newItem} onChange={(e) => setNewItem(e.target.value)} type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <NewTodoForm onSubmit={addTodo} />
       <h1 clasName="header">Todo List</h1>
       <ul className="list">
         {todos.length === 0 && <div className="empty-list">No Items</div>}
